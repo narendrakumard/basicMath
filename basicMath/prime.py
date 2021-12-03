af0097568f83ab0b3dba0848d8eeafeb44039812
narendrakumard <95364513+narendrakumard@users.noreply.github.com>
2021-12-03 10:26:58 -0800

    Add files via upload

---
+++ basicMath/prime.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+factors = 0
+for i in range(2,n):
+    if(n % i == 0):
+        facctors += 1
+if factors == 0:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
