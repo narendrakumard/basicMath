4f87a02f7e304c759e1679cc4c7abd8888b3f8bc
narendrakumard <95364513+narendrakumard@users.noreply.github.com>
2021-12-03 10:35:30 -0800

    Add files via upload

---
+++ basicMath/simple_calculator.py
@@ -0,0 +1,16 @@
+
+a=int(input())
+b=int(input())
+
+sym=input()
+
+if sym=="+":
+    print(a+b)
+elif sym=="-":
+    print(a-b)
+elif sym=="*":
+    print(a*b)
+elif sym=="/":
+    print(a/b)
+elif sym=="%":
+    print(a%b)
