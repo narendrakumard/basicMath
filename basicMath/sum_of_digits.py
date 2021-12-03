46278aae207289568cd8c41916b94ae92a30ec68
narendrakumard <95364513+narendrakumard@users.noreply.github.com>
2021-12-03 10:40:12 -0800

    Add files via upload

---
+++ basicMath/sum_of_digits.py
@@ -0,0 +1,9 @@
+num=input()
+if len(num)==1:
+    print(num)
+elif len(num)==2:
+    print(int(num[0])+int(num[1]))
+elif len(num)==3:
+    print(int(num[0])+int(num[1])+int(num[2]))
+elif len(num)==4:
+    print(int(num[0])+int(num[1])+int(num[2])+int(num[3]))
